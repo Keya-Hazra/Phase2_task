572d09278cea8ec30989fb26c0264a06427ccb7a
shaimatanni~ <tanni@devsstream.com>
2022-09-15 18:34:48 +0600

    change button design

--- src/component/Feedback.jsx
+++ src/component/Feedback.jsx
@@ -5,61 +5,83 @@ import Checkbox from "@mui/material/Checkbox";
 import { useState } from "react";
 
 const Feedback = () => {
-    const [active, setActive] = useState(false)
-    const [okay, setOkay] = useState(false)
-    const [good, setGood] = useState(false)
-    const toggleButton = () => {
-       
-        setActive(active => !active);
-       
-      };
+  const [active, setActive] = useState("btn");
+  const [check, setCheck] = useState("check");
+
+  const toggleButton = (btn) => {
+    setActive((active) => btn);
+  };
+  const toggleCheck = (value) => {
+    setCheck((check) => value);
+  };
   return (
     <>
       <div className="info-box">
-        <div >
+        <div>
           <div className="feed1 feed1-text">
-            <p style={{ marginRight:'9px'}}>01.</p>
+            <p style={{ marginRight: "9px" }}>01.</p>
             <p>How was your day?</p>
           </div>
           <div>
-           
             <div className="d-flex gap-2">
-              <button onClick={()=>toggleButton()} className= {`inactive_button ${active  ? "active_btn" : ""}`}>Bad</button>
-              <button  onClick={()=>toggleButton()} className= {`inactive_button ${active  ? "active_btn" : ""}`}>Okay</button>
-              <button  onClick={toggleButton} className= {`inactive_button ${active ? "active_btn" : ""}`}>Good</button>
+              <button
+                onClick={() => toggleButton("btn1")}
+                className={`inactive_button ${
+                  active === "btn1" ? "active_btn" : ""
+                }`}
+              >
+                Bad
+              </button>
+              <button
+                value={1}
+                onClick={() => toggleButton("btn2")}
+                className={`inactive_button ${
+                  active === "btn2" ? "active_btn" : ""
+                }`}
+              >
+                Okay
+              </button>
+              <button
+                value={3}
+                onClick={() => toggleButton("btn3")}
+                className={`inactive_button ${
+                  active === "btn3" ? "active_btn" : ""
+                }`}
+              >
+                Good
+              </button>
             </div>
           </div>
         </div>
 
         <div className="form_info">
-        <div className="feed1 feed1-text">
-            <p style={{ marginRight:'9px'}}>02.</p>
+          <div className="feed1 feed1-text">
+            <p style={{ marginRight: "9px" }}>02.</p>
             <p>{"  "} Do you feel you’re making difference?</p>
           </div>
-       
+
           <div>
-           
-            <div className="d-flex gap-4 radio_button">
+            <div className="d-flex gap-4 ">
               <label>
-                <button type="button">
+                <button   onClick={() => toggleCheck("check1")} className={`radio_button ${
+                  check === "check1" ? "active_btn" : ""
+                }`}>
                   <FormGroup>
-                    <FormControlLabel
-                      control={<Checkbox defaultChecked />}
-                      label="Yes"
-                    />
+                    <FormControlLabel control={<Checkbox />} label="Yes" />
                   </FormGroup>
                 </button>
               </label>
               <label>
-                <button type="button">
+                <button type="button"  onClick={() => toggleCheck("check2")} className={`radio_button ${
+                  check === "check2" ? "active_btn" : ""
+                }`}>
                   <FormGroup>
-                    <FormControlLabel
-                      control={<Checkbox defaultChecked />}
-                      label="No"
-                    />
+                    <FormControlLabel control={<Checkbox />} label="No"  />
                   </FormGroup>
                 </button>
               </label>
+
+             
             </div>
           </div>
         </div>
